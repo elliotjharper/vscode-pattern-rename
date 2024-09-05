2f455e958620864b01943f0e10ca008fc022373f
elliotjharper <50680194+elliotjharper@users.noreply.github.com>
2024-09-05 22:49:50 +0100

    Diversified the dummy source file

--- src/dummy-source-file.ts
+++ src/dummy-source-file.ts
@@ -10,12 +10,20 @@ export function myThirdFunction(): void {
     console.log('test');
 }
 
+function myLocalFunction(): void {
+    console.log('efwefw');
+}
+
+async function myLocalAsyncFunction(): Promise<void> {
+    console.log('efwefw');
+}
+
 export class Foo {
     public somePublicFunction(): void {
         console.log('test');
     }
 
-    private somePrivateFunction(): void {
+    private async somePrivateBadFunction(): Promise<void> {
         console.log('test');
     }
 }
