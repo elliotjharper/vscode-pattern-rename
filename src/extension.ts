a5c65242c058f045cf2035fdf0d6fe27490083d6
elliotjharper <50680194+elliotjharper@users.noreply.github.com>
2024-09-12 21:38:02 +0100

    Cut content that applied to the extension that i derived this from

--- src/extension.ts
+++ src/extension.ts
@@ -4,39 +4,6 @@ import { getExplorerSelectionFilesList } from './file-utils';
 import { ViewHost } from './rename-view/host';
 import { sortArray } from './sorting';
 
-// async function extractRawSelection(): Promise<void> {
-//     try {
-//         const initialEditorHelper = new ActiveEditorHelper();
-//         const initialSelection = initialEditorHelper.getUserSelection();
-//         const initialSelectionStart = initialEditorHelper.getUserSelectionStartPosition();
-//         const selectedTemplateText = initialEditorHelper.getUserSelectedText();
-//         const initialEditorFilePath = initialEditorHelper.document.fileName;
-//         const initialEditorFileFolder = dirname(initialEditorFilePath);
-
-//         // prompt for new .ts file name
-//         const newTypescriptFileName: string = await askForTypescriptName();
-
-//         // remove selection from the original file
-//         await replaceSelectionInEditor(initialEditorHelper.editor, initialSelection, '');
-
-//         // reformat the original file now content was removed
-//         await triggerFormatDocument(initialEditorHelper.editor);
-
-//         // create the new file using the users selectoutput from the located declaration
-//         // const newFileEditor = await createNewTypescriptFileAndOpen(
-//         //     initialEditorFileFolder,
-//         //     newTypescriptFileName,
-//         //     selectedTemplateText
-//         // );
-
-//         vscode.window.showInformationMessage(
-//             'Finished creating new typescript file with selected content. Exiting...'
-//         );
-//     } catch (err) {
-//         vscode.window.showInformationMessage(`Error whilst extracting typescript: ${err}`);
-//     }
-// }
-
 export function activate(context: vscode.ExtensionContext) {
     let extractRawCommand = vscode.commands.registerCommand(
         'elltg-pattern-rename.beginPatternRename',
--- src/workspace-utils.ts
+++ src/workspace-utils.ts
@@ -9,27 +9,3 @@ export function getWorkspaceRootPath(): string {
 
     return workspaceRootPath;
 }
-
-export async function askForTargetFolder(): Promise<string> {
-    // ask user for folder to create the component under
-    const folderSelection = await vscode.window.showOpenDialog({
-        canSelectFiles: false,
-        canSelectFolders: true,
-        canSelectMany: false,
-    });
-
-    const targetFolder = folderSelection?.[0]?.fsPath;
-
-    if (!targetFolder) {
-        throw new Error('No targetFolder provided. Exiting...');
-    }
-
-    const selectedFolderWithinWorkspace = targetFolder.includes(getWorkspaceRootPath());
-    if (!selectedFolderWithinWorkspace) {
-        throw new Error(
-            'Selected folder was not within the workspace. Seems like a bad selection? Aborting for safety! Exiting...'
-        );
-    }
-
-    return targetFolder;
-}
